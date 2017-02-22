5c83f072f60ccf5f2132a6bb2da6ccffb88da789
davask <admin@davaskweblimited.com>
2017-02-22 14:27:43 +0100

    Update for Theme compatibility

---
+++ web/themes/Exim/src/FrontBundle/EximThemeLoisFrontBundle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of the eXim project.
+ *
+ * (c) david asquiedge <contact@davaskweblimited.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Exim\Theme\Lois\FrontBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EximThemeLoisFrontBundle extends Bundle
+{
+}
