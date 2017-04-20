1f14d0fe0af3a15d76da706e980a7c63dc8c6c17
davask <contact@davaskweblimited.com>
2017-04-20 10:38:54 +0200

    Add themes back

--- web/config.php
+++ web/config.php
@@ -4,9 +4,10 @@ if (!isset($_SERVER['HTTP_HOST'])) {
     exit('This script cannot be run from the CLI. Run it from a browser.');
 }
 
-require_once __DIR__.'/themes/authorizedIps.php';
-
-if (!in_array(@$_SERVER['REMOTE_ADDR'], $authorizedIps)) {
+if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
+    '127.0.0.1',
+    '::1',
+))) {
     header('HTTP/1.0 403 Forbidden');
     exit('This script is only accessible from localhost.');
 }
